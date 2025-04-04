c746fe814da3577104d59b3574ca80a44bfa41fe
MauPrograma777 <160794185+MauPrograma777@users.noreply.github.com>
2025-04-03 18:02:50 -0600

    Update print_n_numbers.py

--- python/print_n_numbers.py
+++ python/print_n_numbers.py
@@ -1,8 +1,8 @@
 try:
-  n=10
-  print("El numero seleccionado fue: " ,n)
+    n = 10
+    print("El número seleccionado fue:", n)
 
-for x in range(n):
-  print(x)
+    for x in range(n):
+        print(x)
 except ValueError:
-  print ("Error: no se puede convertir el parametro proporcionado a entero.")
+    print("Error: no se puede convertir el parámetro proporcionado a entero.")
